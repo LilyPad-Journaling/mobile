bb796862d0ce3df156f5388015dd3e3c9213f462
ebuczek525 <ebuczek@stevens.edu>
2020-10-22 15:22:19 -0400

    Update Settings.js

--- screens/Profile/Settings.js
+++ screens/Profile/Settings.js
@@ -9,16 +9,42 @@ export default function Settings(props) {
     const { navigation } = props;
     return (
         <View style={styles.container}>
-            <Text>Settings</Text>
+            <Text style={styles.text}>Name</Text>
+            
+            <Text style={styles.text}>Phone</Text>
+            <Text style={styles.text}>PIN</Text>
+            <Text style={styles.text}>Color</Text>
+            <View style={styles.Rectangle}/>
+            <Text style={styles.logout}>Logout</Text>
         </View>
+        
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        padding: 20,
         backgroundColor: color.background,
-        alignItems: "center",
-        justifyContent: "center"
-    }
+    },
+    text: {
+        marginTop: 16,
+        paddingVertical: 8,
+        textAlign: "left",
+        fontSize: 20
+    },
+    logout: {
+        marginTop: 300,
+        textAlign: "center",
+        color: "red",
+        textDecorationLine: "underline",
+        fontSize: 20
+    },
+    Rectangle: {
+        marginTop: 20,
+        width: 360,
+        height: 100,
+        backgroundColor: "white"
+       
+        }
   });
