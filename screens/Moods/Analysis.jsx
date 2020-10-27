3da2775dc3edfb734cb8b2ac8f7cc4c818adf2e9
Olivia Powers <powersog@gmail.com>
2020-10-27 15:53:50 -0400

    testing around

--- screens/Moods/Analysis.jsx
+++ screens/Moods/Analysis.jsx
@@ -8,15 +8,50 @@ import { color } from '../../functions/providers/ColorContext';
 export default function Analysis(props) {
     const { navigation } = props;
     return (
+        // <View style = {styles.container}>
+        //     <Text>Mooooood Analysis</Text>
+        //     <TouchableOpacity>
+        //         onPress={() => navigation.navigate('Track')}
+        //         style = {styles.button}
+        //     >
+        //     <Text>Track</Text>
+        //     </TouchableOpacity>
+        // </View>
         <View style={styles.container}>
-            <Text>Mood Analysis</Text>
+            <Text style={{
+                fontSize: 24,
+                color: color.primaryText,
+                marginBottom: 5,
+                textAlign: "left"    
+            }}>
+                Mood</Text>
             <TouchableOpacity
                 onPress={() => navigation.navigate('Track')}
                 style={styles.button}
             >
-                <Text>Track</Text>
+                <Text style={{
+                    fontSize: 40,
+                    color: color.primary
+                }}>
+                    +
+                </Text>
             </TouchableOpacity>
+            <View style = {styles.graph}>
+                <Text>
+                    Test
+                </Text>
+            </View>
         </View>
+
+        // <View style={styles.button}>
+        //     <Text>Mooooood Analysis</Text>
+        //     <TouchableOpacity
+        //         onPress={() => navigation.navigate('Track')}
+        //         style={styles.button}
+        //     >
+        //     <Text>Track</Text>
+        //     </TouchableOpacity>
+        // </View>
     );
 }
 
@@ -28,8 +63,20 @@ const styles = StyleSheet.create({
         justifyContent: "center"
     },
     button: {
+        height: 50,
+        width: 50,
+        borderRadius: 25,
+        backgroundColor: color.highlight,
+        alignItems: "center",
+        justifyContent: "center"
+
+    },
+    graph: {
         height: 100,
         width: 100,
-        backgroundColor: color.primary
+        backgroundColor: color.primary,
+        alignItems: "stretch", 
+        justifyContent: "flex-start"
+
     }
   });
