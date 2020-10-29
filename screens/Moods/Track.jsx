3574e364b4d38b5c4f1475670628d959c59c20e8
bcd2998 <bcd2998@gmail.com>
2020-10-29 16:50:49 -0400

    Added FlatList for Tracking
    Co-authored-by: Olivia Powers <powersog@gmail.com>

--- screens/Moods/Track.jsx
+++ screens/Moods/Track.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
 import {
-    Text, View, TouchableOpacity, StyleSheet
+    Text, View, TouchableOpacity, StyleSheet, FlatList
 } from 'react-native';
 
 import { color } from '../../functions/providers/ColorContext';
+
+const data = [
+    {
+      name: "Rate Your Mood",
+    },
+    {
+      name: "Rate Your Anxiety",
+    },
+    {
+      name: "Rate Your Energy",
+    },
+    {
+        name: "Rate Your Activity"
+    },
+  ];
 
 export default function Track(props) {
     const { navigation } = props;
     return (
         <View style={styles.container}>
             <Text>Track</Text>
+            <FlatList
+        style={{ width: '90%' }}
+        data={data}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => {
+          console.log(item);
+          return (
+            <View>
+              <Text>{item.name}</Text>
+              {/* <View style={styles.graph}>
+                <Text>Graph</Text>
+              </View> */}
+            </View>
+          );
+        }}
+      />
             <TouchableOpacity
                 onPress={() => navigation.goBack()}
                 style={styles.button}
