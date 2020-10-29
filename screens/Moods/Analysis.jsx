6f8c98031162f6cdecdd3d9d5616849fe8216499
Hayden Daly <hdaly1@stevens.edu>
2020-10-29 16:17:16 -0400

    added flatlist to mood analysis

--- screens/Moods/Analysis.jsx
+++ screens/Moods/Analysis.jsx
@@ -1,89 +1,121 @@
-import React from 'react';
+import React from "react";
 import {
-    Text, View, TouchableOpacity, StyleSheet
-} from 'react-native';
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+} from "react-native";
 
-import { color } from '../../functions/providers/ColorContext';
+import { color } from "../../functions/providers/ColorContext";
+
+const data = [
+  {
+    name: "Mood",
+  },
+  {
+    name: "Energy",
+  },
+  {
+    name: "Stress",
+  },
+];
 
 export default function Analysis(props) {
-    const { navigation } = props;
-    return (
-        // <View style = {styles.container}>
-        //     <Text>Mooooood Analysis</Text>
-        //     <TouchableOpacity>
-        //         onPress={() => navigation.navigate('Track')}
-        //         style = {styles.button}
-        //     >
-        //     <Text>Track</Text>
-        //     </TouchableOpacity>
-        // </View>
-        <View style={styles.container}>
-            <Text style={{
-                fontSize: 24,
-                color: color.primaryText,
-                marginBottom: 5,
-                textAlign: "left"    
-            }}>
-                Mood</Text>
-            <TouchableOpacity
-                onPress={() => navigation.navigate('Track')}
-                style={styles.button}
-            >
-                <Text style={{
-                    fontSize: 40,
-                    color: color.primary,
-                    alignItems: "center",
-                    alignContent: "center",
-                    textAlign: "center"
-                }}>
-                    +
-                </Text>
-            </TouchableOpacity>
-            <View style = {styles.graph}>
-                <Text>
-                    Test
-                </Text>
+  const { navigation } = props;
+  return (
+    // <View style = {styles.container}>
+    //     <Text>Mooooood Analysis</Text>
+    //     <TouchableOpacity>
+    //         onPress={() => navigation.navigate('Track')}
+    //         style = {styles.button}
+    //     >
+    //     <Text>Track</Text>
+    //     </TouchableOpacity>
+    // </View>
+    <View style={styles.container}>
+      <Text
+        style={{
+          fontSize: 24,
+          color: color.primaryText,
+          marginBottom: 5,
+          textAlign: "left",
+        }}
+      >
+        Mood
+      </Text>
+      <FlatList
+        style={{ width: '90%' }}
+        data={data}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => {
+          console.log(item);
+          return (
+            <View>
+              <Text>{item.name}</Text>
+              <View style={styles.graph}>
+                <Text>Graph</Text>
+              </View>
             </View>
-        </View>
+          );
+        }}
+      />
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Track")}
+        style={styles.button}
+      >
+        <Text
+          style={{
+            fontSize: 40,
+            color: color.primary,
+            alignItems: "center",
+            alignContent: "center",
+            textAlign: "center",
+          }}
+        >
+            +
+        </Text>
+      </TouchableOpacity>
+    </View>
 
-        // <View style={styles.button}>
-        //     <Text>Mooooood Analysis</Text>
-        //     <TouchableOpacity
-        //         onPress={() => navigation.navigate('Track')}
-        //         style={styles.button}
-        //     >
-        //     <Text>Track</Text>
-        //     </TouchableOpacity>
-        // </View>
-    );
+    // <View style={styles.button}>
+    //     <Text>Mooooood Analysis</Text>
+    //     <TouchableOpacity
+    //         onPress={() => navigation.navigate('Track')}
+    //         style={styles.button}
+    //     >
+    //     <Text>Track</Text>
+    //     </TouchableOpacity>
+    // </View>
+  );
 }
 
 // test 1 2
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: color.background,
-        alignItems: "center",
-        justifyContent: "center"
-    },
-    button: {
-        height: 50,
-        width: 50,
-        borderRadius: 25,
-        backgroundColor: color.highlight,
-        alignItems: "center",
-        justifyContent: "center",
-        position: "absolute", 
-        bottom: 0,
-        right: 0,
-    },
-    graph: {
-        height: 100,
-        width: 100,
-        backgroundColor: color.primary,
-        alignItems: "stretch", 
-        justifyContent: "flex-start"
-
-    }
-  });
+  container: {
+    flex: 1,
+    backgroundColor: color.background,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+    backgroundColor: color.highlight,
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+  },
+  graph: {
+    height: 150,
+    width: '100%',
+    backgroundColor: color.primary,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 25
+  },
+});
