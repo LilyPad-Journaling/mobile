c14ba9315c8f6968ea63c52d2fe6b17bdffb48e1
ebuczek525 <ebuczek@stevens.edu>
2021-02-04 19:50:39 -0500

    Many updates to Journal functionality, needs fixes

--- screens/Journals/JournalList.jsx
+++ screens/Journals/JournalList.jsx
@@ -1,56 +1,29 @@
-import React, { useContext } from "react";
-import {
-  Text,
-  TouchableOpacity,
-  FlatList,
-  View,
-} from "react-native";
+import React, { useContext, useState } from "react";
+import { Text, TouchableOpacity, FlatList, View, Button } from "react-native";
 import { FontAwesome5 as FA5Icon } from "@expo/vector-icons/";
+import { TextInput } from "react-native-gesture-handler";
 
 import { ColorContext } from "../../functions/providers/ColorContext";
+import { UserContext } from "../../functions/providers/UserContext";
 import generalStyles from "../../styles/generalStyles";
 import styles from "../../styles/journalListStyles";
+import Modal from "react-native-modal";
 
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc';
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 dayjs.extend(utc);
-
-// 1EB: OLD function compares today's date to the stored date for a given journal entry, then if false also checks which day of week the journal was created, returns string
-// function dateToDay(date) {
-//   if (new Date().toDateString() === date.toDateString()) {
-//     return "Today";
-//   } else {
-//     switch (date.getDay()) {
-//       case 1:
-//         return "Monday";
-//       case 2:
-//         return "Tuesday";
-//       case 3:
-//         return "Wednesday";
-//       case 4:
-//         return "Thursday";
-//       case 5:
-//         return "Friday";
-//       case 6:
-//         return "Saturday";
-//       default:
-//         return "Sunday";
-//     }
-//   }
-// }
-
-// 2EB: OLD function used to convert firebase-stored timestamp to MDY format for journal list UI
-// function dateToMDY(date) {
-//   let month = date.getUTCMonth() + 1;
-//   let day = date.getUTCDate();
-//   let year = date.getUTCFullYear();
-//   return month + "/" + day + "/" + year;
-// }
 
 function Entry(props) {
   const { color } = useContext(ColorContext);
   const { data } = props;
-  let style = [{...styles.entry, backgroundColor: color.primary, borderColor: color.inactive}];
+  const { pin } = useContext(UserContext);
+  let style = [
+    {
+      ...styles.entry,
+      backgroundColor: color.primary,
+      borderColor: color.inactive,
+    },
+  ];
 
   if (props.style === "top") {
     style.push(styles.entryTop);
@@ -70,17 +43,84 @@ function Entry(props) {
     description = props.body.slice(0, 40) + "...";
   }
 
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+  const Input = (props) => {
+    const { keyboardType, value, onChangeText } = props;
+
+    return (
+      <TextInput
+        numeric
+        keyboardType={keyboardType}
+        style={{
+          fontSize: 20,
+          width: 130,
+          height: 50,
+          borderRadius: 10,
+          borderWidth: 1,
+          borderColor: "black",
+          backgroundColor: "white",
+          justifyContent: "space-between",
+          marginBottom: 10,
+        }}
+        value={value}
+        onChangeText={onChangeText}
+      />
+    );
+  };
+
+  const [inputPin, setPIN] = useState("");
+  const [errorText, setError] = useState("");
+
+  function checkPIN() {
+    if (inputPin == pin) {
+      setError("");
+      toggleModal();
+      props.navigation.navigate("Journal", { data: { ...data } });
+    } else {
+      setError("Invalid PIN entered.");
+    }
+  }
+
   return (
-    <TouchableOpacity
-      onPress={() => props.navigation.navigate("Journal", { data: { ...data }})}
-      style={[style, styles.entryContent]}
-    >
-      <View>
-        <Text style={ {...styles.entryTitle, color: color.primaryText }}>{props.title}</Text>
-        <Text style={{ color: color.inactive}}>{description}</Text>
-      </View>
-      <FA5Icon name='chevron-right' color={color.inactive} size={36} />
-    </TouchableOpacity>
+    <View>
+      <TouchableOpacity
+        onPress={() => {
+          if (props.private) {
+            toggleModal();
+          } else {
+            props.navigation.navigate("Journal", { data: { ...data } });
+          }
+        }}
+        style={[style, styles.entryContent]}
+      >
+        <View>
+          <Text style={{ ...styles.entryTitle, color: color.primaryText }}>
+            {props.title}
+          </Text>
+          <Text style={{ color: color.inactive }}>{description}</Text>
+        </View>
+        <FA5Icon name="chevron-right" color={color.inactive} size={36} />
+      </TouchableOpacity>
+      <Modal isVisible={isModalVisible}>
+        <View style={styles.container}>
+          <Text style={{ marginBottom: 10, fontSize: 20, fontWeight: "bold" }}>Enter PIN</Text>
+          <Text style={{ color: "red", marginBottom: 10 }}>{errorText}</Text>
+          <Input
+            style={styles.input}
+            keyboardType="numeric"
+            value={inputPin}
+            onChangeText={setPIN}
+          />
+          <Button title="Ok" onPress={checkPIN} />
+          <Button title="Cancel" onPress={toggleModal} />
+        </View>
+      </Modal>
+    </View>
   );
 }
 
@@ -92,14 +132,14 @@ function JournalList(props) {
     // If this is the first entry or its date comes before the previous entry's date
     if (
       i === 0 ||
-      dayjs(journals[i].timeCreated).format('MMDDYY') !==
-      dayjs(journals[i - 1].timeCreated).format('MMDDYY')
+      dayjs(journals[i].timeCreated).format("MMDDYY") !==
+        dayjs(journals[i - 1].timeCreated).format("MMDDYY")
     ) {
       // If this is the last entry or its date comes before the next entry's date
       if (
         i === journals.length - 1 ||
-        dayjs(journals[i + 1].timeCreated).format('MMDDYY') !==
-        dayjs(journals[i].timeCreated).format('MMDDYY')
+        dayjs(journals[i + 1].timeCreated).format("MMDDYY") !==
+          dayjs(journals[i].timeCreated).format("MMDDYY")
       ) {
         journals[i].style = "both";
       } else {
@@ -108,8 +148,8 @@ function JournalList(props) {
       // Otherwise, if this is the last entry or its date comes before the next entry's date
     } else if (
       i === journals.length - 1 ||
-      dayjs(journals[i + 1].timeCreated).format('MMDDYY') !==
-      dayjs(journals[i].timeCreated).format('MMDDYY')
+      dayjs(journals[i + 1].timeCreated).format("MMDDYY") !==
+        dayjs(journals[i].timeCreated).format("MMDDYY")
     ) {
       journals[i].style = "bottom";
     }
@@ -120,8 +160,8 @@ function JournalList(props) {
   for (let i = 0; i < journals.length; i++) {
     if (
       data.length === 0 ||
-      dayjs(data[data.length - 1][0].timeCreated).format('MMDDYY') !==
-      dayjs(journals[i].timeCreated).format('MMDDYY')
+      dayjs(data[data.length - 1][0].timeCreated).format("MMDDYY") !==
+        dayjs(journals[i].timeCreated).format("MMDDYY")
     ) {
       data.push([journals[i]]);
     } else {
@@ -131,55 +171,55 @@ function JournalList(props) {
 
   return (
     <>
-    {data.length === 0 ? (
-      <View>
-        <Text>No journals yet, add one below!</Text>
-      </View>
-    ) : (
-      <FlatList
-      data={data}
-      style={styles.topList}
-      contentContainerStyle={styles.entryList}
-      keyExtractor={(item) => item[0].timeCreated + item[0].id}
-      renderItem={({ item }) => (
+      {data.length === 0 ? (
         <View>
-          <Text
-            style={{
-              margin: 4,
-              fontFamily: "medium",
-              color: color.primaryText,
-              fontSize: 16,
-            }}
-          >
-            <Text>
-             {/* 3EB: display day of journal entry */}
-              {dayjs(item[0].timeCreated).format('dddd')}
-              {"  "}
-            </Text>
-            <Text style={{ color: color.inactive, fontSize: 14 }}>
-            {/* 4EB: display MDY of journal entry */}
-              {dayjs(item[0].timeCreated).format('MM/DD/YY')}
-            </Text>
-          </Text>
-          <FlatList
-            style={{ ...generalStyles.shadow, shadowColor: color.shadow }}
-            data={item}
-            keyExtractor={(item) => item.id}
-            renderItem={({ item }) => (
-              <Entry
-                title={item.title}
-                body={item.body}
-                private={item.private}
-                navigation={props.navigation}
-                style={item.style}
-                data={item}
-              />
-            )}
-          />
+          <Text>No journals yet, add one below!</Text>
         </View>
+      ) : (
+        <FlatList
+          data={data}
+          style={styles.topList}
+          contentContainerStyle={styles.entryList}
+          keyExtractor={(item) => item[0].timeCreated + item[0].id}
+          renderItem={({ item }) => (
+            <View>
+              <Text
+                style={{
+                  margin: 4,
+                  fontFamily: "medium",
+                  color: color.primaryText,
+                  fontSize: 16,
+                }}
+              >
+                <Text>
+                  {/* 3EB: display day of journal entry */}
+                  {dayjs(item[0].timeCreated).format("dddd")}
+                  {"  "}
+                </Text>
+                <Text style={{ color: color.inactive, fontSize: 14 }}>
+                  {/* 4EB: display MDY of journal entry */}
+                  {dayjs(item[0].timeCreated).format("MM/DD/YY")}
+                </Text>
+              </Text>
+              <FlatList
+                style={{ ...generalStyles.shadow, shadowColor: color.shadow }}
+                data={item}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                  <Entry
+                    title={item.title}
+                    body={item.body}
+                    private={item.private}
+                    navigation={props.navigation}
+                    style={item.style}
+                    data={item}
+                  />
+                )}
+              />
+            </View>
+          )}
+        />
       )}
-    />
-    )}
     </>
   );
 }
