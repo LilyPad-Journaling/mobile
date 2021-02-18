258649f097a97935d7e0999f11fea213a57004ab
Olivia Powers <powersog@gmail.com>
2021-02-18 15:44:54 -0500

    added all awards  to collection page
    Co-authored-by: Scott Murray <smurray1@stevens.edu>

--- screens/Collection/Collection.jsx
+++ screens/Collection/Collection.jsx
@@ -1,18 +1,29 @@
 import React, { useContext } from "react"
 import { View, Text, FlatList, Image } from "react-native"
 
-import { UserContext } from "../../functions/providers/UserContext"
+import { UserContext } from "../../functions/providers/UserContext";
 import { ColorContext } from "../../functions/providers/ColorContext";
+import { awardsSchemes } from "../../functions/providers/AwardContext";
 
 
 const Collection = () => {
     const { awards } = useContext(UserContext)
     const { color } = useContext(ColorContext)
 
+    let allAwards =  [...awards]
+    const awardIds = allAwards.map(item => item.id)
+    for(let key of Object.keys(awardsSchemes)){
+        if(!awardIds.includes(awardsSchemes[key].id)){
+            let newAward = awardsSchemes[key]
+            newAward.image = "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/78037/lily-pad-clipart-md.png"
+            allAwards.push(newAward)
+        }
+    }
+
     return (
         <View style={{ height: '100%', width: '100%', backgroundColor: color.background }}>
             <FlatList
-                data={awards}
+                data={allAwards}
                 keyExtractor={(item) => item.id}
                 renderItem={({ item }) => (
                     <View style={{ width: '100%', height: 70, flexDirection: 'row', justifyContent: 'flex-start' }}>
