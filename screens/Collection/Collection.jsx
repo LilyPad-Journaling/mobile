12e0b6672d6121d4e14e53daefa01d67bf669924
Angelina Zaccaria <angelina.zaccaria@gmail.com>
2021-03-16 16:01:17 -0400

    fixed line wrapping for awards
    Co-authored-by: Scott Murray <smurray1@stevens.edu>
    Co-authored-by: Erik Buczek <ebuczek@stevens.edu>

--- screens/Collection/Collection.jsx
+++ screens/Collection/Collection.jsx
@@ -37,7 +37,7 @@ const Collection = () => {
                 renderItem={({ item }) => (
                     <View style={{ width: '95%', height: 70, flexDirection: 'row', justifyContent: 'flex-start' }}>
                         <Image style={{ height: 50, width: 50, marginHorizontal: 20, marginVertical: 10 }} source={{ uri: item.image}} />
-                        <Text style={{ marginTop: 23, fontSize: 16, color: color.primaryText }}>{item.description}</Text>
+                        <Text style={{ marginTop: 23, fontSize: 16, color: color.primaryText, flexWrap: 'wrap', flexShrink: 1}}>{item.description}</Text>
                     </View>
                 )}
                 renderSectionHeader={({ section: { title } }) => (
